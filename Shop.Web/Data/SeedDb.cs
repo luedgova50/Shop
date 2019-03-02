8c6142d5f77c147caad9a7c8c5295de9613691ab
Luis Eduardo González Vargas <luedgova1967@hotmail.com>
2019-03-02 14:11:04 -0500

    Implement Repository Pattern

---
+++ Shop.Web/Data/SeedDb.cs
@@ -0,0 +1,49 @@
+﻿namespace Shop.Web.Data
+{
+    
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+
+            random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await context.Database.EnsureCreatedAsync();
+
+            if (!context.Products.Any())
+            {
+                AddProduct("Plana");
+                AddProduct("Fileteadora");
+                AddProduct("Collarin");
+                AddProduct("20 U");
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = random.Next(1000000),
+                IsAvailabe = true,
+                Stock = random.Next(100)
+            });
+        }
+
+    }
+}
--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -31,6 +31,9 @@ namespace Shop.Web
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDb>();
+
+            services.AddScoped<IRepository, Repository>();
 
             services.Configure<CookiePolicyOptions>(options =>
             {
